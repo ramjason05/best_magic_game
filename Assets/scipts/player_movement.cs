c59bb781f8f51219590b05660436c12a879cdc93
ramjason05 <ramjason05@gmail.com>
2026-03-30 22:04:55 -0700

    ovement_script

--- Assets/scipts/player_movement.cs
+++ Assets/scipts/player_movement.cs
@@ -1,10 +1,21 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int health = 100;
+    public float moveSpeed = 8f;
+    public float jumpForce = 10f;
+    
+
     void Start()
     {
+        public void Foward()
+        {
+            
+        }
         
     }
 
